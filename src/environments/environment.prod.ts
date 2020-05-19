ca7c41793bff71d0a19a08c14419caef2db653fd
kdfemi <39603632+kdfemi@users.noreply.github.com>
2020-05-19 21:23:18 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  googleMapsAPIKey: 'AIzaSyA8cSf02YZE_E2iaDF_MYEJ9KCaP1mZlL8',
-  firebaseAPIKey: 'AIzaSyC7tTfdJEHUVygE3rT04kn04Lj3qCd7oNY'
+  googleMapsAPIKey: '',
+  firebaseAPIKey: ''
 };
